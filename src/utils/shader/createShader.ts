a184d17b2a1905746ad2b4a4b4fb78a6b7f35208
bqxbqx <boqingxin14@gmail.com>
2024-10-11 14:10:41 +0800

    feat ✨: complete move node

---
+++ src/utils/shader/createShader.ts
@@ -0,0 +1,25 @@
+function createShader(
+  gl: WebGL2RenderingContext,
+  type: number,
+  source: string
+) {
+  const shader = gl.createShader(type);
+
+  if (!shader) {
+    console.error("failed to create shader, type" + type);
+    return null;
+  }
+
+  gl.shaderSource(shader, source);
+  gl.compileShader(shader);
+
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    console.error(gl.getShaderInfoLog(shader));
+    gl.deleteShader(shader);
+    return null;
+  }
+
+  return shader;
+}
+
+export default createShader;
